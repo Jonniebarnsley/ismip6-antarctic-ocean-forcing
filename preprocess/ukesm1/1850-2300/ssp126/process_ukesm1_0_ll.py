7a0512d0596be00b24e9f9647a23941821def157
Jonnie <jonniebarnsley@gmail.com>
2025-07-15 14:44:41 +0100

    refactored into smaller functions and updated xarray syntax for more modern versions

--- preprocess/ukesm1/1850-2300/ssp126/process_ukesm1_0_ll.py
+++ preprocess/ukesm1/1850-2300/ssp126/process_ukesm1_0_ll.py
@@ -1,36 +1,33 @@
 #!/usr/bin/env python
 
 import argparse
-import xarray
-import numpy
+import xarray as xr
+import numpy as np
 import os
+import logging
 import warnings
-
-parser = argparse.ArgumentParser(
-    description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('-o', dest='out_dir', metavar='DIR', required=True,
-                    type=str, help='output directory')
-args = parser.parse_args()
+from pathlib import Path
+from xarray.coders import CFDatetimeCoder
 
 
-def compute_yearly_mean(inFileName, outFileName):
-    # crop to below 48 S and take annual mean over the data
-    if os.path.exists(outFileName):
+def compute_yearly_mean(in_file: Path, out_file: Path, lat_threshold: float = -48.0):
+    """Compute annual mean for data south of a latitude threshold and save to NetCDF."""
+    if out_file.exists():
         return
-    print('{} to {}'.format(inFileName, outFileName))
+    logging.info(f"Processing {in_file} to {out_file}")
 
-    ds = xarray.open_dataset(inFileName)
+    coder = CFDatetimeCoder(use_cftime=True)
+    ds = xr.open_dataset(in_file, decode_times=coder)
     ds = ds.rename({'longitude': 'lon',
                     'latitude': 'lat',
                     'vertices_longitude': 'lon_vertices',
                     'vertices_latitude': 'lat_vertices'})
-
-    ds = ds.drop('time_bnds')
+    ds = ds.drop_vars('time_bnds')
 
     # crop to Southern Ocean
     minLat = ds.lat.min(dim='i')
-    mask = minLat <= -48.
-    yIndices = numpy.nonzero(mask.values)[0]
+    mask = minLat <= lat_threshold
+    yIndices = np.nonzero(mask.values)[0]
     ds = ds.isel(j=yIndices)
 
     for coord in ['lev_bnds', 'lon_vertices', 'lat_vertices']:
@@ -43,7 +40,7 @@ def compute_yearly_mean(inFileName, outFileName):
 
     # convert back to CF-compliant time
     ds = ds.rename({'year': 'time'})
-    ds['time'] = 365.0*ds.time
+    ds['time'] = 365.0 * ds.time
     ds.time.attrs['bounds'] = "time_bnds"
     ds.time.attrs['units'] = "days since 0000-01-01 00:00:00"
     ds.time.attrs['calendar'] = "noleap"
@@ -51,82 +48,104 @@ def compute_yearly_mean(inFileName, outFileName):
     ds.time.attrs['long_name'] = "time"
     ds.time.attrs['standard_name'] = "time"
 
-    timeBounds = numpy.zeros((ds.sizes['time'], ds.sizes['bnds']))
+    timeBounds = np.zeros((ds.sizes['time'], ds.sizes['bnds']))
     timeBounds[:, 0] = ds.time.values
     timeBounds[:, 1] = ds.time.values + 365
     ds['time_bnds'] = (('time', 'bnds'), timeBounds)
+    ds['time_bnds'].attrs['units'] = "days since 0000-01-01 00:00:00"
+    ds['time_bnds'].attrs['calendar'] = "noleap"
 
-    ds = xarray.decode_cf(ds, use_cftime=True)
+    time_var = ds.variables['time']
+    bnds_var = ds.variables['time_bnds']
+    decoded_time = coder.decode(time_var, time_var.attrs)
+    decoded_bnds = coder.decode(bnds_var, bnds_var.attrs)
+    ds = ds.assign_coords(time=decoded_time, time_bnds=decoded_bnds)
 
     encoding = {'time': {'units': 'days since 0000-01-01'}}
-    ds.to_netcdf(outFileName, encoding=encoding)
+    ds.to_netcdf(out_file, encoding=encoding)
 
 
-model = 'UKESM1-0-LL'
-run = 'r4i1p1f2'
+def process_periods(dates, model, run, scenario, out_dir, file_type):
+    """Process a set of periods for a given variable and scenario."""
+    files = []
+    for date in dates:
+        in_file = out_dir / f"{file_type}_Omon_{model}_{scenario}_{run}_gn_{date}.nc"
+        out_file = out_dir / f"{file_type}_annual_{model}_{scenario}_{run}_{date}.nc"
+        compute_yearly_mean(in_file, out_file)
+        files.append(out_file)
+    return files
 
-dates = {'thetao': ['195001-199912',
-                    '200001-201412'],
-         'so': ['195001-199912',
-                '200001-201412']}
 
-histFiles = {}
-for field in dates:
-    histFiles[field] = []
-    for date in dates[field]:
-        inFileName = '{}/{}_Omon_{}_historical_{}_gn_{}.nc'.format(
-            args.out_dir, field, model, run, date)
+def combine_files(hist_files: list[Path], scenario_files: list[Path], out_file: Path, start_year: int):
+    """Combine historical and scenario files into a single NetCDF file for a given period."""
+    if out_file.exists():
+        return
+    logging.info(f"Combining files into {out_file}")
+    coder = CFDatetimeCoder(use_cftime=True)
+    ds = xr.open_mfdataset(hist_files + scenario_files, combine='nested', concat_dim='time', decode_times=coder)
+    mask = ds['time.year'] >= start_year
+    tIndices = np.nonzero(mask.values)[0]
+    ds = ds.isel(time=tIndices)
+    encoding = {'time': {'units': 'days since 0000-01-01'}}
+    ds.to_netcdf(out_file, encoding=encoding)
 
-        outFileName = '{}/{}_annual_{}_historical_{}_{}.nc'.format(
-            args.out_dir, field, model, run, date)
 
-        compute_yearly_mean(inFileName, outFileName)
-        histFiles[field].append(outFileName)
+def main():
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-o', dest='out_dir', metavar='DIR', required=True, type=str, help='output directory')
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    out_dir = Path(args.out_dir)
 
-dates = {'thetao': ['201501-204912',
-                    '205001-209912',
-                    '210001-210012',
-                    '210101-214912',
-                    '215001-219912',
-                    '220001-224912',
-                    '225001-229912',
-                    '230001-230012'],
-         'so': ['201501-204912',
-                '205001-209912',
-                '210001-210012',
-                '210101-214912',
-                '215001-219912',
-                '220001-224912',
-                '225001-229912',
-                '230001-230012']}
-for scenario in ['ssp126']:
-    scenarioFiles = {}
-    for field in dates:
-        scenarioFiles[field] = []
-        for date in dates[field]:
-            inFileName = '{}/{}_Omon_{}_{}_{}_gn_{}.nc'.format(
-                args.out_dir, field, model, scenario, run, date)
+    model = 'UKESM1-0-LL'
+    run = 'r4i1p1f2'
+    scenario = 'ssp126'
 
-            outFileName = '{}/{}_annual_{}_{}_{}_{}.nc'.format(
-                args.out_dir, field, model, scenario, run, date)
+    hist_dates = {
+        'thetao': [
+            '195001-199912',
+            '200001-201412'
+            ], 
+        'so': [
+            '195001-199912',
+            '200001-201412'
+            ]
+        }
+    scenario_dates = {
+        'thetao': [
+            '201501-204912', 
+            '205001-209912', 
+            '210001-210012', 
+            '210101-214912', 
+            '215001-219912', 
+            '220001-224912', 
+            '225001-229912', 
+            '230001-230012'
+            ],
+        'so': [
+            '201501-204912', 
+            '205001-209912', 
+            '210001-210012', 
+            '210101-214912', 
+            '215001-219912', 
+            '220001-224912', 
+            '225001-229912', 
+            '230001-230012'
+            ]
+    }
 
-            compute_yearly_mean(inFileName, outFileName)
-            scenarioFiles[field].append(outFileName)
+    hist_files = {}
+    scenario_files = {}
+    for var in hist_dates:
+        hist_files[var] = process_periods(hist_dates[var], model, run, 'historical', out_dir, var)
+    for var in scenario_dates:
+        scenario_files[var] = process_periods(scenario_dates[var], model, run, scenario, out_dir, var)
+    
+    start_year = 1995
+    for var in ['so', 'thetao']:
+        out_file = out_dir / f"{var}_annual_{model}_{scenario}_{run}_{start_year}01-230012.nc"
+        combine_files(hist_files[var], scenario_files[var], out_file, start_year)
 
-    for field in ['so', 'thetao']:
-        outFileName = \
-            '{}/{}_annual_{}_{}_{}_199501-230012.nc'.format(
-                args.out_dir, field, model, scenario, run)
-        if not os.path.exists(outFileName):
-            print(outFileName)
 
-            # combine it all into a single data set
-            ds = xarray.open_mfdataset(histFiles[field] + scenarioFiles[field],
-                                       combine='nested', concat_dim='time',
-                                       use_cftime=True)
-
-            mask = ds['time.year'] >= 1995
-            tIndices = numpy.nonzero(mask.values)[0]
-            ds = ds.isel(time=tIndices)
-            encoding = {'time': {'units': 'days since 0000-01-01'}}
-            ds.to_netcdf(outFileName, encoding=encoding)
+if __name__ == "__main__":
+    main()
